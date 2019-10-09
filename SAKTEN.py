9212af17f7f9a3f8a0b43e94a14b2935d1de455a
ElmerDivi <divproxima@gmail.com>
2019-10-09 17:50:49 +0530

    Shortened comments

--- SAKTEN.py
+++ SAKTEN.py
@@ -9,10 +9,12 @@ try:
         # exit the program if not
         raise sys.exit()
 except:
-    # exit when no input is given and this catches EOF(end of file) exception
+    # exit when no input is given
+    # and this catches EOF(end of file) exception
     sys.exit()
 
-# Initialize N,M,Q to keep track of the sum of n,m,q (should not exceed 3*10**5)
+# Initialize N,M,Q to keep track of the sum of n,m,q
+# (should not exceed 3*10**5)
 N,M,Q = 0,0,0
 # iterate over test cases
 while(t):
@@ -30,11 +32,12 @@ while(t):
     M += m
     Q += q
 
-    # if the sum exceeds 3*10**5 ,stop execution
+    # if the sum exceeds 3*10**5, stop execution
     if N > 3*(10**5) or M > 3*(10**5) or Q > 3*(10**5):
         raise sys.exit()
 
-    # dictionary to keep track of which row,col gets repeated and how many times
+    # dictionary to keep track of which row,col gets repeated
+    # and how many times
     row, col = {},{}
     # intialized to zero
     num_odd_row, num_odd_col = 0,0
@@ -45,7 +48,8 @@ while(t):
         try:
             # r for row and c for column
             r,c = map(int,input().split())
-            # if r and c out of matrix dimentions then exit program
+            # if r and c out of matrix dimentions
+            # then exit program
             if r<1 or r>n or c<1 or c>m:
                 raise sys.exit()
             # catches eof exception
@@ -64,15 +68,18 @@ while(t):
             if rcheck and ccheck:
                 col[c] += 1
                 row[r] += 1
-            # else increment r, is the new row, set it to one and increment c by one
+            # else increment r, is the new row, set it to one
+            # and increment c by one
             elif ccheck:
                 col[c] += 1
                 row[r] = 1
-            # else increment c, is the new col, set it to one and increment r by one
+            # else increment c, is the new col, set it to one
+            # and increment r by one
             elif rcheck:
                 row[r] += 1
                 col[c] = 1
-            # this will not happen when both r and c are new because the condition doesn't allow
+            # this will not happen when both r and c are new
+            # because the condition doesn't allow
             else:
                 pass
         # if both points are new
@@ -84,7 +91,9 @@ while(t):
 
     # number of odd rows or columns
     for i in row.values():
-        # dictionary is (key,value) pair , for every value in row check if odd increment by one
+        # dictionary is (key,value) pair
+        # for every value in row check if odd
+        # if so, increment by one
         if i%2 == 1:
             # counts all odd rows
             num_odd_row += 1
@@ -92,6 +101,7 @@ while(t):
     for i in col.values():
         if i%2 == 1:
             num_odd_col += 1
-    # total odd points = oddrow * evencol + oddcol * evenrow
+    # total odd points =
+    # oddrow * evencol + oddcol * evenrow
     oddpts = (n-num_odd_row)*num_odd_col + (m-num_odd_col)*num_odd_row
     print(oddpts)
