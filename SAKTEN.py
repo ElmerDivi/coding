98fcbea9325abe5d4da0c652dddc27acd09a058b
ElmerDivi <divproxima@gmail.com>
2019-10-09 13:20:01 +0530

    Cleaned comments

--- SAKTEN.py
+++ SAKTEN.py
@@ -1,68 +1,95 @@
-import sys   # to exit the program when the user input is not correct 
-try:  
+# to exit the program when the user input is not correct
+import sys
+try:
   t = int(input())
-  if t>=1 and t<=300: # test input in these limits are aloud
+  # test input in these allowed limits
+  if t>=1 and t<=300:
     pass
   else:
-    raise sys.exit()  # otherwise exit the program
+    # exit the program if not
+    raise sys.exit()
 except:
-  sys.exit() # exit when user not given any input and enter this catches EOF(end of file) exception 
+  # exit when no input is given and this catches EOF(end of file) exception
+  sys.exit()
 
-N,M,Q = 0,0,0  # this is initialize of N,M,Q to keep track the sum of n,m,q not exceed 3*10**5
-while(t): # interate over test cases
+# Initializes N,M,Q to keep track of the sum of n,m,q (should not exceed 3*10**5)
+N,M,Q = 0,0,0
+# iterate over test cases
+while(t):
   t -= 1
   try:
     n,m,q = map(int,input().split())
-    if n<1 or n>10**5 or m<1 or m>10**5 or q<1 or q>10**5: # values of n,m,q if out of constraint then exit.
+    # if values of n,m,q out of constraint then exit.
+    if n<1 or n>10**5 or m<1 or m>10**5 or q<1 or q>10**5:
       raise sys.exit()
-  except: # catches EOF exception
+  # catches EOF exception
+  except:
     sys.exit()
-  N += n  # as explained above 
+  # as explained above
+  N += n
   M += m
   Q += q
-   
-  if N > 3*(10**5) or M > 3*(10**5) or Q > 3*(10**5): # if the sum exceeds the 3*10**5 it stops execution
-      raise sys.exit() 
-    
-  row, col = {},{}  # the dictionary to keep track of which row,col get repeated and how many times 
-  num_odd_row, num_odd_col = 0,0 # intialized to zero
-  
-  while q:  # iterate for all the operations
+
+  # if the sum exceeds 3*10**5 ,stop execution
+  if N > 3*(10**5) or M > 3*(10**5) or Q > 3*(10**5):
+      raise sys.exit()
+
+  # dictionary to keep track of which row,col gets repeated and how many times
+  row, col = {},{}
+  # intialized to zero
+  num_odd_row, num_odd_col = 0,0
+
+  # iterate all the operations
+  while q:
     q -= 1
     try:
-      r,c = map(int,input().split()) # r for row and c for column
-      if r<1 or r>n or c<1 or c>m: # if r and c out of matrix dimentions then exit program
+      # r for row and c for column
+      r,c = map(int,input().split())
+      # if r and c out of matrix dimentions then exit program
+      if r<1 or r>n or c<1 or c>m:
         raise sys.exit()
-    except: # catches eof exception
+    # catches eof exception
+    except:
       sys.exit()
-    
-    # checks if the row is already in row or col dictionary 
-    rcheck = (r in row.keys()) # rcheck is boolean value true or false
-    ccheck = (c in col.keys()) # similarly ccheck for col
-    
+
+    # checks if the row is already in row or col dictionary
+    # rcheck is boolean value true or false
+    rcheck = (r in row.keys())
+    # similarly ccheck for col
+    ccheck = (c in col.keys())
+
     # if any of row or column is repeated
-    if rcheck or ccheck: # if any of row or column is repeated
-      if rcheck and ccheck: # if both repeated increment both by one 
+    if rcheck or ccheck:
+      # if both repeated increment both by one
+      if rcheck and ccheck:
         col[c] += 1
         row[r] += 1
-      elif ccheck: # else increment r is new row set it to one and increment c by one 
+      # else increment r, is the new row, set it to one and increment c by one
+      elif ccheck:
         col[c] += 1
         row[r] = 1
-      elif rcheck: # else increment c is new col set it to one and increment r by one 
+      # else increment c, is the new col, set it to one and increment r by one
+      elif rcheck:
         row[r] += 1
         col[c] = 1
-      else: # this never gone come in it when both r and c are new  becouse very condtion to enter this doesn't allow
+      # this will not happen when both r and c are new because the condition doesn't allow
+      else:
         pass
    # if both points are new
     else:
-      row[r] = 1 # set r to one
-      col[c] = 1 # set c to one
-  
+      # set r to one
+      row[r] = 1
+      # set c to one
+      col[c] = 1
+
   # number of odd rows or columns
   for i in row.values():
-    if i%2 == 1: # dictionary is (key,value) pair , for every value in row check if odd increment by one 
-      num_odd_row += 1 # counts all odd rows
-  for i in col.values(): # same thing for col
+    # dictionary is (key,value) pair , for every value in row check if odd increment by one
+    if i%2 == 1:
+      # counts all odd rows
+      num_odd_row += 1
+  # same thing for col
+  for i in col.values():
     if i%2 == 1:
       num_odd_col += 1
   # total odd points : oddrow * evencol + oddcol * evenrow
